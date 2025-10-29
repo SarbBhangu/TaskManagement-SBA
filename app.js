3698d8552ec1a3b04211e8323fe7b4490862d918
SarbBhangu <Bhangu513@gmail.com>
2025-10-29 10:42:20 -0400

    added and called all input and filter IDs using getelementbyid for all

---
+++ app.js
@@ -0,0 +1,10 @@
+const nameInput = document.getElementById('taskName');
+const categoryInput = document.getElementById('taskCategory');
+const deadlineInput = document.getElementById('taskDeadline');
+const statusInput = document.getElementById('taskStatus');
+const addBtn = document.getElementById('addTaskBtn');
+const filterStatus= document.getElementById('filterStatus');
+const filterCategory= document.getElementById('filterCategory');
+const applyFiltersBtn= document.getElementById('applyFiltersBtn');
+const resetFiltersBtn= document.getElementById('resetFiltersBtn');
+const taskList= document.getElementById('taskList');

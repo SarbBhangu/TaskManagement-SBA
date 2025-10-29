1d99f3c629f05be607da55f337a723db2d4b8ebf
SarbBhangu <Bhangu513@gmail.com>
2025-10-29 11:26:04 -0400

    added new variable that displays the li under tasks on the webpage

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const nameInput = document.getElementById('taskName');
 const categoryInput = document.getElementById('taskCategory');
 const deadlineInput = document.getElementById('taskDeadline');
 const statusInput = document.getElementById('taskStatus');
-const addBtn = document.getElementById('addTaskBtn');
+const addBtn = document.getElementById('addBtn');
 const filterStatus= document.getElementById('filterStatus');
 const filterCategory= document.getElementById('filterCategory');
 const applyFiltersBtn= document.getElementById('applyFiltersBtn');
@@ -15,5 +15,10 @@ function addTask() {
     let newTask = nameInput.value;
     tasks.push(newTask);
     console.log(tasks);
+
+    let newItem = document.createElement("li");
+    newItem.textContent= newTask;
+    taskList.appendChild(newItem);
+    nameInput.value= "";
 }
 addBtn.addEventListener("click", addTask);
